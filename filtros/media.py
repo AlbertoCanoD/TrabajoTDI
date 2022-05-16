b26a4966a6de79512a049b2d38ae0501865da848
Alberto Cano Delgado <canoalboroteo@gmail.com>
2022-05-16 15:46:19 +0200

    Filtro media

--- filtros/media.py
+++ filtros/media.py
@@ -3,50 +3,48 @@ import cv2
 import matplotlib.pyplot as plt
 import argparse
 
-#To execute .\gris.py -i NOMBREIMAGEN
+#To execute .\media.py -i NOMBREIMAGEN -k KERNELSIZE
 
 # Argument parser.
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
 	help="Image path or name if in the same folder")
 
-ap.add_argument("-k", "--kernel", help="Kernel size")
+ap.add_argument("-k", "--kernel", type=int, help="Kernel size, > 3")
 
 args = vars(ap.parse_args())
 
 # Read the image and convert to grayscale
 image = cv2.imread(args["image"], cv2.IMREAD_COLOR)
 
-#path = r'C:\Users\Alberto\Downloads\TrabajoTDI\filtros\test.png'
-#image = cv2.imread(path)
-
 # Check if not exist
 if image is None:
     print("Image not found")
     exit(0)
 
-# Read the kernel size
-if(args["kernel"] != 0) :
-    kernel = args["kernel"]
-else :
-    kernel = 3
+# Processing kernel size, 
+ksize = args["kernel"]
 
-#kernel = 3
+if ksize < 3 :
+    kernel = (3,3)
+else:
+    kernel = (args["kernel"],args["kernel"])
 
-fig,ax = plt.subplots(1,1,figsize=(10,10))
-ax1 = plt.subplot(121)
-ax2 = plt.subplot(122)
+blur = cv2.blur(image,kernel)
 
 # Image
-ax1.imshow(image,cmap=plt.cm.gray)
-ax1.set_title('Imagen original')
-ax1.axis("off")
+plt.subplot(121)
+plt.imshow(image)
+plt.title("Imagen original")
+plt.xticks([]), plt.yticks([])
 
 # Histogram of image
-blur = cv2.blur(image,(kernel,kernel))
-ax2.imshow(blur)
-ax2.set_title('Imagen con filtro media con kernel ' + kernel + ' x ' + kernel)
-ax2.axis("off")
+plt.subplot(122)
+plt.imshow(blur)
+if ksize < 3 :
+    plt.title('Filtro media con kernel 3x3')
+plt.title('Filtro media con kernel ' + str(ksize) + 'x' + str(ksize))
+plt.xticks([]), plt.yticks([])
 
 plt.show()
 
