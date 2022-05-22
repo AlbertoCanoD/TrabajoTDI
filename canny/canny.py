92a01ea694187d118b0cb82bea29f3c2be91139a
Alberto Cano Delgado <canoalboroteo@gmail.com>
2022-05-22 12:34:42 +0200

    Canny finished

--- canny/canny.py
+++ canny/canny.py
@@ -3,7 +3,7 @@ import cv2
 import matplotlib.pyplot as plt
 import argparse
 
-# To execute .\canny.py -i IMAGEPATH -m (INT) -M (INT)
+# To execute .\canny.py -i IMAGEPATH -m minthreshold -M maxthreshold
 
 # Argument parser.
 ap = argparse.ArgumentParser()
